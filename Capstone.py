8ee68e55562d4d1a5e059342c524be097a02998f
mostwanted-786 <60353780+mostwanted-786@users.noreply.github.com>
2021-02-11 12:56:14 +0530

    proj

---
+++ Capstone.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Notebook will be used for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone project")
+
